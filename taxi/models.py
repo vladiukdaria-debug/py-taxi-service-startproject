7e0337f193222c14d6b8b4a1758c7c05ef6a41c0
vladiukdaria-debug <vladiukdaria@gmail.com>
2026-06-05 23:26:26 +0200

    solution

--- taxi/models.py
+++ taxi/models.py
@@ -3,35 +3,30 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Driver(AbstractUser):
-    license_number = models.CharField(max_length=128, unique=True)
-
-    class Meta:
-        verbose_name = "driver"
-        verbose_name_plural = "drivers"
-
-
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    country = models.CharField(max_length=128)
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100)
 
-    class Meta:
-        verbose_name = "manufacturer"
-        verbose_name_plural = "manufacturers"
+    def __str__(self):
+        return self.name
 
 
 class Car(models.Model):
-    model = models.CharField(max_length=128)
+    model = models.CharField(max_length=100)
     manufacturer = models.ForeignKey(
-        Manufacturer,
-        on_delete=models.CASCADE,
-        related_name="cars"
+        Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
-        related_name="drivers"
+        related_name="cars"
     )
 
-    class Meta:
-        verbose_name = "car"
-        verbose_name_plural = "cars"
+    def __str__(self):
+        return self.model
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.username
